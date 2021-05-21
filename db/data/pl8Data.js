69a3ee60b9162449536605f4397dfb6e865ec006
Chris Wood <cawoodlfc@hotmail.co.uk>
2021-05-21 15:31:49 +0100

    Added seeding

---
+++ db/data/pl8Data.js
@@ -0,0 +1,12 @@
+export default [
+  {
+    name: 'meat foot',
+    origin: 'nightmares',
+    description: 'meaty',
+    image: 'image fo foot',
+    ingredients: ['meat', 'onion'],
+    recipe: 'recipe',
+    prepTime: 10,
+    cookTime: 10,
+  }
+]
