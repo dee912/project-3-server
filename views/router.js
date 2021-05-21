25fb1374770e5df3d154f2a3dd60774349306382
Chris Wood <cawoodlfc@hotmail.co.uk>
2021-05-21 16:32:56 +0100

    views

--- views/router.js
+++ views/router.js
@@ -1,11 +1,8 @@
 import express from 'express'
 
 import pl8Controller from '../controllers/pl8s.js'
-<<<<<<< HEAD
-=======
 import m8sController from '../controllers/m8s.js'
 // import commentController from '../controllers/comments'
->>>>>>> development
 
 const router = express.Router()
 
